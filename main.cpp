304011a0af5168f87423499c06de9e2b51c6efb0
k-pocius <pockristijonas@gmail.com>
2025-10-15 21:26:35 +0300

    T function

--- main.cpp
+++ main.cpp
@@ -3,19 +3,58 @@
 
 
 int main(){
-    const string s = "KRISTIS5";
-    vector<int> A;
-    for (char c : s){
-        A.push_back(c);
-        cout << int(c) << " ";
-    }
+    vector<uint32_t> A;
+    ASCII(A);
+    // for(int x : A){
+    //     cout << x << " ";
+    // }
+    // cout << endl;
+    sum(A); 
+    // for(int x : A){
+    //     cout << x << " ";
+    // }
+    cout << endl;   
     vector<string> B(A.size());
-    cout << endl;
-    sum(A);
+
+    
     K(A);
+    // for(int x : A){
+    //     cout << x << " ";
+    // }
+    // cout << endl;
     R(A);
+    // for(int x : A){
+    //     cout << x << " ";
+    // }
+    // cout << endl;
     I(A);
-    S(A, B, 8);
+    // for(int x : A){
+    //     cout << x << " ";
+    // }
+    // cout << endl;
+    // cout << endl;
+    S(A,B,8);
+    // for(int x : A){
+    //     cout << x << " ";
+    // }
+    // cout << endl;
+    // for (string x : B){
+    //     cout << x;
+    // }
+    // cout << endl;
+    // cout << endl;
+    T(A,B,8);
+    //     for(int x : A){
+    //     cout << x << " ";
+    // }
+    // cout << endl;
+    // for (string x : B){
+    //     cout << x;
+    // }
+
+    I(A);
+    S(A,B,8);
+    cout << endl;
     for (string x : B){
         cout << x;
     }
--- main.h
+++ main.h
@@ -1,22 +1,28 @@
-#include "lib.h"
 
-void sum(vector<int> & A){
-    string a = "";
-    int j = 0;
+const string s = "KRISTIS5";
+
+void ASCII(vector<uint32_t> &A){
+    for(char c : s){
+        A.push_back(uint32_t(c));
+    }
+}
+
+void sum(vector<uint32_t> &A){
+    string a = "this is simple";
     for(int i = 0; i < a.length(); i++){
         for(int x = 0; x < 3; x++){
-            A[(i + x) % A.size()] += int(a[i]);
+            A[(i + x) % A.size()] += uint32_t(a[i]);
         }
     }
 }
 
-void K(vector<int> & A){ //“K” is like a key that unlocks and expands differences.
+void K(vector<uint32_t> &A){
     for(int i = 0; i < A.size(); i++){
-        A[i] *= i + 1;
+        A[i] *= (i + 1);
     }
 }
 
-void R(vector<int> & A){ // R-rotate bits
+void R(vector<uint32_t> &A){
     int n;
     for(int i = 0; i < A.size(); i++){
         n = (i % 3) + 1;
@@ -24,36 +30,65 @@ void R(vector<int> & A){ // R-rotate bits
     }
 }
 
-void I(vector<int> & A){ // I-invert bits XOR
+void I(vector<uint32_t> &A){
     int n = A.size();
-    for(int j = 0; j < 2; j++){
+    for(int j = 0; j < 4; j++){
         for(int i = 0; i < A.size(); i++){
             A[i] = (A[i] ^ A[(i + 1) % n]) + (A[(i + 2) % n] ^ A[(i + 3) % n]);
         }
     }
 }
 
-void S(vector<int> & A, vector<string> & B, int length){ // S-shape hex format
-    int number;
-    char a;
+void S(vector<uint32_t> &A, vector<string> &B, int length){
+    if(B.size() < A.size()) B.resize(A.size());
+    
     for(int i = 0; i < A.size(); i++){
-        number = A[i];
+        B[i].clear();
+        uint32_t number = A[i];
+        string temp;
         for(int j = 0; j < length; j++){
-            if (number % 16 < 10)
-                a = '0' + (number % 16);
-            else {
-                switch(number % 16) {
-                    case 10: a = 'A'; break;
-                    case 11: a = 'B'; break;
-                    case 12: a = 'C'; break;
-                    case 13: a = 'D'; break;
-                    case 14: a = 'E'; break;
-                    case 15: a = 'F'; break;
-                }
-            }
-            B[i] += a;
-            number = number / 16;
+            int nibble = number % 16;
+            char a = (nibble < 10) ? '0' + nibble : 'A' + (nibble - 10);
+            temp += a;
+            number /= 16;
         }
-        reverse(B[i].begin(), B[i].end());
+        reverse(temp.begin(), temp.end());
+        B[i] = temp;
     }
 }
+
+void T(vector<uint32_t> &A, vector<string> &B, int length){
+    string hexString;
+    vector<uint32_t> Tnums;
+    vector<uint32_t> Knums;
+
+    for(string x : B) hexString += x;
+
+    // convert hex characters to 0-15 integers
+    for(char c : hexString){
+        if(isdigit(c)) Tnums.push_back(c - '0');
+        else Tnums.push_back(c - 'A' + 10);
+    }
+
+    // key ASCII
+    ASCII(Knums);
+
+    // twist digits
+    for(int i = 0; i < Tnums.size(); i++){
+        Tnums[i] = (Tnums[i] + (Knums[i % 7] % 16)) % 16;
+    }
+
+    // repack 64 digits into 8 big numbers
+    vector<uint32_t> newA;
+    for(int i = 0; i < Tnums.size(); i += 8){
+        uint32_t num = 0;
+        for(int j = 0; j < 8; j++){
+            num = (num << 4) | Tnums[i + j];
+        }
+        newA.push_back(num);
+    }
+
+    A = newA;
+    B.resize(A.size());
+}
+
